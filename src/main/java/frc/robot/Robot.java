2836770acd1e335efb8ca696ae6aceaf833c5379
BobSaidHi <47067448+BobSaidHi@users.noreply.github.com>
2022-01-28 16:24:57 -0800

    Actually works - added everything

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,9 +4,12 @@
 
 package frc.robot;
 
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 
 /**
@@ -14,9 +17,16 @@ import edu.wpi.first.wpilibj.motorcontrol.Spark;
  * arcade steering.
  */
 public class Robot extends TimedRobot {
-  private final Spark m_leftMotor = new Spark(0);
-  private final Spark m_rightMotor = new Spark(2);
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
+  private final Spark m_leftMotor1 = new Spark(0);
+  private final Spark m_leftMotor2 = new Spark(1);
+  private final Spark m_rightMotor1 = new Spark(2);
+  private final Spark m_rightMotor2 = new Spark(3);
+
+  private final MotorControllerGroup leftMotors = new MotorControllerGroup(m_leftMotor1, m_leftMotor2);
+  private final MotorControllerGroup rightMotors = new MotorControllerGroup(m_rightMotor1, m_rightMotor2);
+
+  private final DifferentialDrive m_robotDrive = new DifferentialDrive(leftMotors, rightMotors);
+
   private final Joystick m_stick = new Joystick(0);
 
   @Override
@@ -24,7 +34,12 @@ public class Robot extends TimedRobot {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
-    //m_rightMotor.setInverted(true);
+    rightMotors.setInverted(true);
+
+    //UsbCamera cam = CameraServer.getInstance().startAutomaticCapture();
+    UsbCamera cam = CameraServer.startAutomaticCapture();
+    cam.setResolution(160, 120);
+    cam.setFPS(30);
   }
 
   @Override
